8080140867efb0e599acb81353012620a29e2bf6
Titov Dmitry <dtitov94@gmail.com>
2018-11-26 17:00:36 +0300

    Initial commit

---
+++ src/main/java/d/e/titov/springuserservice/SpringUserServiceApplication.java
@@ -0,0 +1,12 @@
+package d.e.titov.springuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserServiceApplication.class, args);
+	}
+}
